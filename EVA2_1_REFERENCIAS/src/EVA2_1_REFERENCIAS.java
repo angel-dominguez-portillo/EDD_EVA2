b0713780dfe223d3b11771aa6acd2b5e1560581b
angel-dominguez-portillo <L24550319@chihuahua2.tecnm.mx>
2026-03-11 17:09:37 -0600

    Add files via upload

---
+++ EVA2_1_REFERENCIAS/src/EVA2_1_REFERENCIAS.java
@@ -0,0 +1,20 @@
+
+public class EVA2_1_REFERENCIAS {
+	public static void main(String args[]){
+		Ejemplo miObj = new Ejemplo();
+		miObj.valor = 100;
+		miObj.otro = new Ejemplo();
+		miObj.otro.valor = 200;
+		miObj.otro.otro = new Ejemplo();
+		miObj.otro.otro.valor = 300;
+		System.out.println("MiObj = " + miObj.valor);
+		System.out.println("MiObj.otro = " + miObj.otro.valor);
+		System.out.println("MiObj.otro.otro = " + miObj.otro.otro.valor);
+	}
+	
+
+}
+class Ejemplo{
+	int valor;
+	Ejemplo otro;
+}
