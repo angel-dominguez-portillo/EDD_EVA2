8bea313e6a138f8e94ea71f66d40ee7cedacf040
angel-dominguez-portillo <L24550319@chihuahua2.tecnm.mx>
2026-03-11 17:52:36 -0600

    Update EVA2_1_REFERENCIAS.java

--- EVA2_1_REFERENCIAS/src/EVA2_1_REFERENCIAS.java
+++ EVA2_1_REFERENCIAS/src/EVA2_1_REFERENCIAS.java
@@ -7,9 +7,15 @@ public class EVA2_1_REFERENCIAS {
 		miObj.otro.valor = 200;
 		miObj.otro.otro = new Ejemplo();
 		miObj.otro.otro.valor = 300;
-		System.out.println("MiObj = " + miObj.valor);
+		/*System.out.println("MiObj = " + miObj.valor);
 		System.out.println("MiObj.otro = " + miObj.otro.valor);
-		System.out.println("MiObj.otro.otro = " + miObj.otro.otro.valor);
+		System.out.println("MiObj.otro.otro = " + miObj.otro.otro.valor);*/
+		Ejemplo temp = miObj;
+		while(temp != null) {
+			System.out.println("Valor = " + temp.valor);
+			temp = temp.otro;
+			
+		}
 	}
 	
 
@@ -17,4 +23,5 @@ public class EVA2_1_REFERENCIAS {
 class Ejemplo{
 	int valor;
 	Ejemplo otro;
+
 }
