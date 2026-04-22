d95a219291552feafd9ccb23097855d15a2361d9
angel-dominguez-portillo <L24550319@chihuahua2.tecnm.mx>
2026-04-22 15:06:40 -0600

    Add files via upload

---
+++ EVA2_4_STACK/src/EVA2_4_STACK.java
@@ -0,0 +1,25 @@
+
+public class EVA2_4_STACK {
+	public static void main(String args[]){
+		Pila pila = new Pila();
+		pila.push(100);
+		pila.push(200);
+		pila.push(300);
+		pila.push(400);
+		pila.push(500);
+		System.out.println("");
+		pila.imprimirListaInv();
+		System.out.println("[" + pila.peek() + "]");
+		pila.imprimirLista();
+		System.out.println("");
+		System.out.println("adios al :" + pila.pop());
+		pila.imprimirLista();
+		pila.imprimirListaInv();
+		System.out.println("Cantidad de nodos: " + pila.longitud());
+		System.out.println("adios al :" + pila.pop());
+		pila.imprimirLista();
+		pila.imprimirListaInv();
+		System.out.println("Cantidad de nodos: " + pila.longitud());
+		pila.push(900);
+	}//hacer ahora en heap la practica 5
+}
